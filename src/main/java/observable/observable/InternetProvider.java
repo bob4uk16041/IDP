72e60dfd815a22c56f8194fec728e206bb2f9ad4
bob4uk16041 <bob4ukspdukr@gmail.com>
2017-09-03 21:23:17 +0300

    new change

--- src/main/java/observable/observable/InternetProvider.java
+++ src/main/java/observable/observable/InternetProvider.java
@@ -6,9 +6,7 @@ import java.util.Observer;
 public class InternetProvider implements Observer {
     @Override
     public void update(Observable o, Object arg) {
-        if (true) {
-            News news = (News) arg;
-            System.out.println("InternetProvider: " + news.toString());
-        }
+        News news = (News) arg;
+        System.out.println("InternetProvider: " + news.toString());
     }
 }
--- src/main/java/observable/observable/News.java
+++ src/main/java/observable/observable/News.java
@@ -22,4 +22,24 @@ public class News {
                 ", description='" + description + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        News news = (News) o;
+
+        if (!name.equals(news.name)) return false;
+        if (!date.equals(news.date)) return false;
+        return description.equals(news.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + date.hashCode();
+        result = 31 * result + description.hashCode();
+        return result;
+    }
 }
