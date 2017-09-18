cffc3b145f3d7d2508d2071baea52bbb65673a20
bob4uk16041 <bob4ukspdukr@gmail.com>
2017-09-18 09:26:57 +0300

    fix reent exp

--- src/main/java/FutureTask.java
+++ src/main/java/FutureTask.java
@@ -1,3 +1,11 @@
+/*
+ For the Future API: please write sample 
+application that will perform some dummy 
+computation, pass this computation to 2 second future; 
+second future should process the result of computation from the first future. Meanwhile launch some 
+computation with in the 3rd future, and write 4th future that will process the results from 2nd and 3rd 
+computations
+*/
 import java.util.concurrent.*;
 
 
@@ -23,41 +31,70 @@ public class FutureTask {
     }
 
     public static void main(String [] args) throws ExecutionException, InterruptedException {
-        Integer t = task();
+        task();
     }
 
     public static Integer task() throws ExecutionException, InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(4);
+//
+//        Callable<Integer> task1 = () -> {
+//                System.out.println("Run task1");
+//                TimeUnit.SECONDS.sleep(2);
+//                return factorial(10);
+//        };
+//        Future<Integer> future1 = executor.submit(task1);
+//
+//        Callable<Integer> task2 = () -> {
+//            System.out.println("Run task2");
+//            Integer sum = sum(150);
+//            sum += future1.get();
+//            return sum;
+//        };
+//        Future<Integer> future2 = executor.submit(task2);
+//
+//        Callable<Integer> task3 = () -> {
+//            System.out.println("Run task3");
+//            return factorial(3);
+//        };
+//        Future<Integer> future3 = executor.submit(task3);
+//
+//        Callable<Integer> task4 = () -> {
+//            System.out.println("Run task4");
+//            return future3.get() + future2.get();
+//        };
+//        Future<Integer> future4 = executor.submit(task4);
+//
+//        Integer result = future4.get();
+        
+        
+        CompletableFuture<Integer> cf1 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("Run task1");
+//            TimeUnit.SECONDS.sleep(2);
+            return 5;//factorial(5);
+        }, executor);
 
-        Callable task1 = () -> {
-                System.out.println("Run task1");
-                TimeUnit.SECONDS.sleep(2);
-                return factorial(10);
-        };
-        Future<Integer> future1 = executor.submit(task1);
-
-        Callable task2 = () -> {
-            System.out.println("Run task2");
-            Integer sum = sum(150);
-            sum += future1.get();
-            return sum;
-        };
-        Future<Integer> future2 = executor.submit(task2);
-
-        Callable task3 = () -> {
+   //     CompletableFuture<Integer> cf2 = cf1.thenRun(() -> {System.out.println("r t 2");}).thenApply(f -> f + sum(5));
+        CompletableFuture<Integer> cf2 = cf1.thenApply(f -> {
+        	return f + sum(3);
+        });   
+        CompletableFuture<Integer> cf3 = CompletableFuture.supplyAsync(() -> {
             System.out.println("Run task3");
-            return factorial(3);
-        };
-        Future<Integer> future3 = executor.submit(task3);
+            return 10;//factorial(4);
+        }, executor);
+        
+        CompletableFuture<Integer> cf4 = cf2.thenApply(f -> {
+			try {
+				return f + cf3.get();
+			} catch (InterruptedException | ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return f;
+		});
 
-        Callable task4 = () -> {
-            System.out.println("Run task4");
-            return future3.get() + future2.get();
-        };
-        Future<Integer> future4 = executor.submit(task4);
-
-        Integer result = future4.get();
-        System.out.println("result: " + result);
+        Integer result = cf4.get();
+        
+        System.out.println("result: " + result); //5! + 5+ +4! = 159
         return result;
     }
 
--- src/main/java/ReentrantLocksExample.java
+++ src/main/java/ReentrantLocksExample.java
@@ -1,73 +1,117 @@
+/*
+ "Please write dummy application that would demonstrate usage of ReentrantLocks feature. Be ready to explain it.(for instance : write simple
+applications, launch 2 threads consurrently, change state in share variable in thread-1 and process those change in thread-2; modify state(value) of shared
+object in thread-2 and return the control to thread-1 )"
+
+ */
+
+
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class ReentrantLocksExample {
 
     public static void main(String[] args) {
-
-        Resource commonResource= new Resource();
-        ReentrantLock locker = new ReentrantLock();
-
-        Thread t1 = new Thread(new CountThread(commonResource, locker));
+        System.out.println("Start-----------------------");
+        Count count = new Count();
+        
+        Thread t1 = new Thread(new Minuss(count));
         t1.setName("Thread "+ 1);
         t1.start();
 
-        Thread t2 = new Thread(new Count(commonResource, locker));
+        Thread t2 = new Thread(new Pluss(count));
         t2.setName("Thread "+ 2);
         t2.start();
 
     }
     static class Resource{
         int share=0;
+        boolean state = true;
+    }
+    
+    static class Minuss  implements Runnable {
+    	Count count;
+    	
+    	Minuss(Count count){
+    		this.count = count;
+    	}
+
+		@Override
+		public void run() {
+			for (int i = 1; i < 6; i++) {
+				count.minus();
+			}
+		}
     }
 
-    static class CountThread implements Runnable{
+    
+    static class Pluss implements Runnable {
+    	Count count;
+    	
+    	Pluss(Count count){
+    		this.count = count;
+    	}
+    	
+		@Override
+		public void run() {
+			for (int i = 1; i < 6; i++) {
+				count.plus();
+			}
+		}
+    }
+    
+    
+    static class Count{
 
         Resource res;
         ReentrantLock locker;
-        CountThread(Resource res, ReentrantLock lock){
-            this.res=res;
-            locker = lock;
+        Condition condition;
+        Count(){
+            this.res = new Resource();
+            locker =  new ReentrantLock();;
+            this.condition = locker.newCondition();
+
         }
-        public void run(){
+        public void plus(){
             locker.lock();
             try{
-                for (int i = 1; i <= 5; i++){
-                    res.share += 2;
-                    System.out.println(Thread.currentThread().getName()+ " " + res.share);
-                    Thread.sleep(1000);
-                }
+            	while (res.state) {
+            		condition.await();
+            	}
+            	res.share += Math.round(Math.random()*20);
+                System.out.println(Thread.currentThread().getName()+ " " + res.share + " state="+res.state);
+                res.state = !res.state;
+                condition.signalAll();
+
             }
             catch(InterruptedException e){
                 System.out.println(e.getMessage());
             }
             finally{
                 locker.unlock();
+                
             }
         }
-    }
-
-    static class Count implements Runnable{
-        Resource res;
-        ReentrantLock locker;
-        Count(Resource res, ReentrantLock lock){
-            this.res=res;
-            locker = lock;
-        }
-        public void run(){
+        public void minus(){
             locker.lock();
+          
             try{
-                    for (int i = 1; i <= 5; i++){
-                        res.share += 3;
-                        System.out.println(Thread.currentThread().getName()+ " " + res.share);
+            	  while (!res.state) {
+              		condition.await();
+              	}
 
-                        Thread.sleep(100);
-                    }
+        	res.share -= Math.round(Math.random()*10);;
+            System.out.println(Thread.currentThread().getName()+ " " + res.share + " state="+res.state);
+            res.state = !res.state;
+
+                condition.signalAll();
             }
             catch(InterruptedException e){
                 System.out.println(e.getMessage());
             }
             finally{
                 locker.unlock();
+            	
             }
         }
     }
